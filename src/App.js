460d0bf04c9bba716856d07a3a3292335937c11d
Nicholas Herrick <nth.herrick@gmail.com>
2020-05-14 16:35:20 -0600

    added content, added styling, removed react logo images, added google font and added favicon

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import Logo from './assets/img/logo.jpg';
+import Product1 from './assets/img/product1.jpg';
 import './App.css';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <div className='logo'>
+        <img className='logo-image' src={Logo} alt='' />
+      </div>
+      <div className='product'>
+        <img className='product1-image' src={Product1} alt='' />
+      </div>
+      <div className='text'>
+        <h2>1627</h2>
+      </div>
+    </div>
+  );
 }
 
 export default App;
